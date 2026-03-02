abfceb1b0b0137068c23223df75caad377ca3f88
S-r-i-j-a-n <srijannrpp@gmail.com>
2026-03-02 16:40:33 +0000

    node test cases

---
+++ nodeapp/models/StartupSubmission.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const startupSubmissionSchema = new mongoose.Schema(
+    {
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true, ref: "User"
+        },
+        userName: { type: String, required: true },
+        startupProfileId: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true, ref: "StartupProfile"
+        },
+
+        submissionDate: {
+            type: Date
+        },
+        marketPotential: {
+            type: Number
+        },
+        launchYear: {
+            type: Date
+        },
+        expectedFunding: {
+            type: Number
+        },
+        status: {
+            type: Number
+        },
+        address: {
+            type: String
+        },
+
+        pitchDeckFile: {
+            type: String,
+            required: true
+        },
+    },
+    { timestamps: true }
+);
+
+module.exports =
+    mongoose.models.StartupSubmission ||
+    mongoose.model("StartupSubmission", startupSubmissionSchema);
