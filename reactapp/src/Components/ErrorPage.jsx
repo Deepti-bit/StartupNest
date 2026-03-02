c90e3fd758f037a1f19ae66db3fec88dcb70ab44
autodidact28 <mayankvkulshrestha@gmail.com>
2026-03-02 19:19:37 +0000

    Initial step of testcase passing done

---
+++ reactapp/src/Components/ErrorPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ErrorPage = () => {
+  return (
+    <div>
+      <h1>Oops! Something Went Wrong</h1>
+
+      <p>Please try again later.</p>
+    </div>
+  );
+};
+
+export default ErrorPage;
